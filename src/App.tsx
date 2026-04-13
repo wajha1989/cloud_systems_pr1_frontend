63ebd0daf3a0ae654bd73bc07324148536a62f14
Mateusz Omiecinski <wajha.1989@interia.pl>
2026-04-13 02:13:15 +0200

    added cognito integration, dummy urls for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import { useState } from "react";
+import {useEffect, useState} from "react";
 import HabitForm from "./components/HabitForm";
 import HabitList from "./components/HabitList";
 import HabitDetails from "./components/HabitDetails";
+import { login, logout } from "./auth/Cognito";
 
 export default function App() {
+    useEffect(() => {
+        const hash = window.location.hash;
+
+        if (hash.includes("id_token")) {
+            const params = new URLSearchParams(hash.substring(1));
+
+            const token = params.get("id_token");
+
+            if (token) {
+                localStorage.setItem("id_token", token);
+            }
+
+            window.location.hash = "";
+        }
+    }, []);
     const [selectedHabitId, setSelectedHabitId] = useState<number | null>(null);
     const [refresh, setRefresh] = useState(0);
 
@@ -18,6 +34,8 @@ export default function App() {
             {selectedHabitId && (
                 <HabitDetails habitId={selectedHabitId} />
             )}
+            <button onClick={login}>Login</button>
+            <button onClick={logout}>Logout</button>
         </div>
     );
 }
--- src/api/api.ts
+++ src/api/api.ts
@@ -1,13 +1,18 @@
 import axios from "axios";
+import {getToken} from "../auth/Cognito.tsx";
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
 
-// Later: attach Cognito JWT here
-// API.interceptors.request.use(config => {
-//   config.headers.Authorization = "Bearer TOKEN";
-//   return config;
-// });
+API.interceptors.request.use((config) => {
+    const token = getToken();
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+});
 
 export default API;
