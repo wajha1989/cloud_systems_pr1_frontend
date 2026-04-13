69e26833279add953b4ac4e98149539792db3295
Mateusz Omiecinski <wajha.1989@interia.pl>
2026-04-13 09:48:19 +0200

    changed url to normal value, should work

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,9 +1,15 @@
 import axios from "axios";
 import {getToken} from "../auth/Cognito.tsx";
 
+// TODO change this if I ever get to building this app in cloud envrinoment
+// const API = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL
+// });
+
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
+    baseURL: "habit-backend-env.eba-asjncbp3.us-east-1.elasticbeanstalk.com"
 });
+
 
 API.interceptors.request.use((config) => {
     const token = getToken();
